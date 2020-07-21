d0f675d970846f68a9a421768f904230cb6cf62e
yuejianjian <yuejianjian@kuaixiaohulian.com>
2020-07-21 18:14:27 +0800

    添加登录页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,43 @@
 <template>
 	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
-		<view @click="clickTest">跳转</view>
-		<uni-badge text="1"></uni-badge>
-		<uni-badge text="2" type="success" @click="bindClick"></uni-badge>
-		<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
-		<view class="example-body message">
-			<button class="button" type="primary" @click="toggle('bottom')"><text class="button-text">底部弹出</text></button>
+		<view class="selGysDiv" v-if="gysStatus">
+			<view class="selContent" @click="toggle('bottom')">
+				<view class="selWd">
+					切换经销商
+				</view>
+				<view class="selGysName">︵
+				<text>{{gysname}}</text>︶</view>
+			</view>
+			<view class="selBottom"></view>
 		</view>
+		<view class="login-icon">
+			  <block v-if="gysdataslist.gysLoginUrl">
+				<image class="login-img" mode="widthFix" :src="gysdataslist.gysLoginUrl"></image>
+			  </block>
+			  <block v-else>
+				<image class="login-img" mode="widthFix" src="../../static/image/logo.png"></image>
+			  </block>
+		</view>	
+		<view class="gysname" v-if="gysStatus">{{gysname}}</view>
+		<view class="list">
+			<image class="icon_n1" src="../../static/image/login_username.png"></image>
+			<input type="text" placeholder="手机号" maxlength="11" />
+			<image class="icon-guanbi" src="../../static/image/close.png"></image>
+		</view>
+		<view class="list">
+			<image class="icon_n2" src="../../static/image/login_locked.png"></image>
+			<input type="password" placeholder="密码"/>
+			<image class="icon-guanbi" src="../../static/image/close.png"></image>
+		</view>
+		<view class="position">
+			<view class="forget_password">忘记密码</view>
+		</view>
+		 <button size="large" class='loginbtn'>登录</button>
 		<uni-popup ref="popup" :type="type" :animation="false" @change="change">
-			<view class="popup-content">popup 内容，此示例没有动画效果</view>
+			<view class="popup-content" v-for="(item,index) in gysarr" :key="index" @click="selectgys(item)">
+				{{item.organizationName}}
+			</view>
+			
 		</uni-popup>
 	</view>
 </template>
@@ -29,16 +55,32 @@
 			return {
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
 				type: 'top',
+				gysarr:[],
+				gysStatus:true,
+				gysname:'',
+				gys_no:null,
+				gysdataslist:{},
+				userphone:'',
 			}
 		},
-		
+		mounted:function(){
+			this.clickTest();
+		},
 		methods: {
 			toggle(type) {
+				console.log(type);
 				this.type = type
 				this.$refs.popup.open()
 			},
 			change(e) {
 				console.log('popup ' + e.type + ' 状态', e.show)
+			},
+			selectgys(val){
+				console.log(val);
+				this.$refs.popup.close();
+				this.gysname=val.organizationName;
+				this.gys_no=val.organizationNo;
+				this.gysdataslist =val;
 			},
 			clickTest(){
 				console.log(11111)
@@ -51,8 +93,9 @@
 				        }						
 				this.$myRequest(params).then(res=>{
 					console.log(res,444444444444444);					
-				if(res.data.code == 200){
-					  this.goods = that.data.goods.concat(res.data.data)
+				if(res.statusCode == 200){
+					  this.gysarr = res.data.data;
+					  console.log(this.gysarr);		
 				  }
 				}) 
 						
@@ -71,8 +114,126 @@
 		line-height: 24px;
 		
 	}
+	.selGysDiv {
+	    position: fixed;
+	    top: 0;
+	    right: 0;
+	    width: 60px;
+	    background: #1E90FF;
+	    height: max-content;
+	}
+	
+	
+	.selBottom {
+	    width: 0;
+	    height: 0;
+	    border-top: 20px solid #1E90FF;
+	    border-right: 25px solid white;
+	    border-bottom: 40px solid white;
+	    border-left: 35px solid white;
+	}
+	
+	.selContent {
+	    height: max-content;
+	    overflow: hidden;
+	    color: #fff;
+	    padding: 8px 8px 5px;
+	}
+	
+	.selWd {
+	    border: 1px solid white;
+	    font-size: 13px;
+	    float: left;
+	    width: 20px;
+	    text-align: center;
+	    word-break: break-all;
+	}
+	
+	.selGysName {
+	    float: right;
+	    width: 11px;
+	    font-size: 11px;
+	    word-break: break-word;
+	    line-height: 12px;
+	    text-align: center;
+	}
+	.login-icon{
+	  width: 160rpx;
+	  height: 160rpx;
+	  margin: 0 auto;
+	  margin-top: 150rpx;
+	  margin-bottom:30rpx;
+	}
+	.login-img{
+	  width: 160rpx;
+	  height: 160rpx;
+	  /* border-radius: 110rpx; */
+	  /* opacity: 0.6; */
+	}
+	.gysname{
+	  width:100%;
+	  height:40px;
+	  line-height: 40px;
+	  text-align: center;
+	  color:#1E90FF;
+	  font-size: 13px;
+	}
+	.list {
+	    display: flex;
+	    -webkit-box-orient: horizontal;
+	    -moz-box-orient: horizontal;
+	    -ms-box-orient: horizontal;
+	    -o-box-orient: horizontal;
+	    box-orient: horizontal;
+	    position: relative;
+	    margin-top: 1.71875rem;
+	    border-bottom: 1px solid #1e90ff;
+	}
+	
+	.list image {
+	    margin-right: 10px;
+	    margin-top: 8px;
+	    width: 20px;
+	    height: 20px;
+	    display: inline-block;
+	}
+	.list input {
+	    flex: 1;
+	    display: block;
+	    border: none;
+	    height: 40px;
+	    line-height: 40px;
+	    outline: none;
+	    font-size: 0.875rem;
+	    background: none;
+	}
+	.position {
+	    width:100%;
+	    height:40px;
+	    line-height: 40px;
+	    text-align: right;
+	}
+	.forget_password {
+	    width:100%;
+	    height:40px;
+	    color: #1e90ff;
+	    font-size: 12px;
+	}
+	.loginbtn{
+	  background:#1E90FF;
+	  color:#fff;
+	  width:100%;
+	  height:40px;
+	  border-radius: 20px;
+	  font-size: 15px;
+	}
 	.popup-content {
+		width:100%;
+		height:40px;
+		line-height: 40px;
 		background-color: #fff;
-		padding: 15px;
+		font-size: 14px;
+		border-bottom: 1px solid #eee;
+		text-align: center;
 	}
 </style>
